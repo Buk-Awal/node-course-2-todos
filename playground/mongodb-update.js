1d8743a4b51b024f4f9d2cafe546e7e4b2e3de68
Awal <awalbukari3@gmail.com>
2017-09-11 22:17:09 +0000

    Add update script

---
+++ playground/mongodb-update.js
@@ -0,0 +1,39 @@
+//const MongoClient = require('mongodb').MongoClient;
+const {MongoClient, ObjectID} = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+  if(err) {
+    return console.log('Unable to connect to MongoDB server');
+  }
+  console.log('Connected to MongoDB server');
+
+  //findOneAndUpdate
+  // db.collection('Todos').findOneAndUpdate({
+  //   _id: new ObjectID('59b6fbff6630a78896d0ef2f')
+  // }, {
+  //   $set: {
+  //     completed: true
+  //   }
+  // }, {
+  //   returnOriginal: false
+  // }).then((result) => {
+  //   console.log(result);
+  // });
+
+  db.collection('Users').findOneAndUpdate({
+    _id: new ObjectID('59b67d2727c90712bca398b6')
+  }, {
+    $set: {
+      name: 'Bukari'
+    },
+    $inc: {
+      age: 1
+    }
+  }, {
+    returnOriginal: false
+  }).then((result) => {
+    console.log(result);
+  });
+
+  //db.close();
+});
